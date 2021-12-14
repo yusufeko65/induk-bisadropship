e556e326488222ca0c677917b056103681dfec21
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2021-12-14 21:08:32 +0700

    first commit

---
+++ Modules/Page/Routes/public.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('{slug}', 'PageController@show');
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Route::get('install/pre-installation', 'InstallController@preInstallation');
+Route::get('install/configuration', 'InstallController@getConfiguration');
+Route::post('install/configuration', 'InstallController@postConfiguration');
+Route::get('install/complete', 'InstallController@complete');
+*/
